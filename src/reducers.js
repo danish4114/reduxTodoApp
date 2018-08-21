f688b0be14e6f15bd56d5d1467cea9293463944c
danish4114 <mohd_danish@excellencetechnologies.in>
2018-08-21 11:35:33 +0530

    implemented redux-actions

--- src/reducers.js
+++ src/reducers.js
@@ -1,47 +1,47 @@
-import Redux from "react-redux";
-import { CHECKED, ADD_TODO, itemID, EDITTODO, DELETETODO } from "./actions";
-import { combineReducers } from "redux";
+import { completed, addTodo, editTodo, deleteTodo } from "./actions";
+import { handleActions } from "redux-actions";
 import _ from "lodash";
 
-const initialState = [
+const defaultState = [
   { id: 1, name: "abc", completed: false },
   { id: 2, name: "abc", completed: false }
 ];
-const todoReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_TODO:
-      return [
-        ...state,
-        {
-          id: itemID,
-          name: action.text,
-          completed: false
-        }
-      ];
-    case EDITTODO:
-      let clone = _.clone(state);
-      clone[action.index].name = action.newValue;
-      return clone;
-
-    case DELETETODO:
-      let newCLone = _.clone(state).slice();
-      newCLone.splice(action.index, 1);
-      return newCLone;
-
-    case CHECKED:
-      return state.map((todo, index) => {
-        if (index == action.index) {
-          return Object.assign({}, todo, {
-            completed: !todo.completed
-          });
-        }
-        return todo;
-      });
-    default:
-      return state;
-  }
+const toggletodo = (state = defaultState, { payload: { id } }) => {
+  return _.map(state, (todo, index) => {
+    if (index == id) {
+      return Object.assign({}, todo, { completed: !todo.completed });
+    }
+    return todo;
+  });
 };
-const statusReducer = combineReducers({
-  todoReducer
-});
+const EditTodo = (state = defaultState, { payload: { newValue, index } }) => {
+  let clone = state;
+  clone[index].name = newValue;
+  return clone;
+};
+let itemID = 3;
+const Addtodo = (state = defaultState, { payload: { text } }) => {
+  return [
+    ...state,
+    {
+      id: itemID++,
+      name: text,
+      completed: false
+    }
+  ];
+};
+const DeleteTodo = (state = defaultState, { payload: { index } }) => {
+  let newCLone = _.clone(state).slice();
+  newCLone.splice(index, 1);
+  return newCLone;
+};
+const statusReducer = handleActions(
+  {
+    [completed]: toggletodo,
+    [editTodo]: EditTodo,
+    [addTodo]: Addtodo,
+    [deleteTodo]: DeleteTodo
+  },
+  defaultState
+);
 export default statusReducer;
